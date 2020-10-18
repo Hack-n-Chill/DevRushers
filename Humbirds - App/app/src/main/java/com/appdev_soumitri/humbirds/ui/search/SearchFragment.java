e5658d08a1b37ee647467f468c83e3c727f415c7
soumitri2001 <56516406+soumitri2001@users.noreply.github.com>
2020-10-18 06:43:50 +0530

    Play Music feature with Search and added .apk and Screenshots

--- Humbirds - App/app/src/main/java/com/appdev_soumitri/humbirds/ui/search/SearchFragment.java
+++ Humbirds - App/app/src/main/java/com/appdev_soumitri/humbirds/ui/search/SearchFragment.java
@@ -1,8 +1,13 @@
 package com.appdev_soumitri.humbirds.ui.search;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -10,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,6 +33,7 @@ import android.widget.Toast;
 
 import com.android.volley.VolleyError;
 import com.appdev_soumitri.humbirds.MainActivity;
+import com.appdev_soumitri.humbirds.NoInternetActivity;
 import com.appdev_soumitri.humbirds.R;
 import com.appdev_soumitri.humbirds.Urls;
 import com.appdev_soumitri.humbirds.models.SearchSongAdapter;
@@ -38,18 +45,23 @@ import com.appdev_soumitri.humbirds.services.RequestCodes;
 import com.appdev_soumitri.humbirds.services.RequestResponse;
 import com.appdev_soumitri.humbirds.services.ResponseService;
 import com.appdev_soumitri.humbirds.services.TrackRequest;
+import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 import retrofit2.http.Url;
 
 public class SearchFragment extends Fragment implements RequestResponse {
+
+
 
     public static SearchFragment newInstance() {
         return new SearchFragment();
@@ -69,7 +81,20 @@ public class SearchFragment extends Fragment implements RequestResponse {
     private CustomLLManager customLinearLayoutManager;
     private SearchSongAdapter searchSongAdapter;
 
+    SharedPreferences sharedPreferences;
+    ArrayList<String> history=new ArrayList<>();
+
+    private Toolbar songToolbar;
+    private TextView mSelectedTrackTitle;
+    private ImageView mSelectedTrackImage;
+
+    private MediaPlayer mMediaPlayer;
+    private ImageView mPlayerControl;
+
+
     private TrackRequest trackRequest;
+
+    public SearchFragment() {}
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -98,8 +123,43 @@ public class SearchFragment extends Fragment implements RequestResponse {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                // play the song using MediaPlayer
+                if(!(new NetworkConnection(getContext()).isConnected())) {
+                    startActivity(new Intent(getContext(), NoInternetActivity.class));
+                }
 
+                // play the song using MediaPlayer
+                songToolbar.setVisibility(View.VISIBLE);
+
+                SongModel track = songs.get(position);
+
+                mSelectedTrackTitle.setText(track.getTitle());
+
+                history.add(Long.toString(track.getId()));
+                HashSet<String> hs=new HashSet<>(history);
+                sharedPreferences.edit().putStringSet("song_id",hs).apply();
+
+                Glide.with(requireActivity())
+                        .load(track.getArtworkURL())
+                        .into(mSelectedTrackImage);
+
+                Log.d("Status","play song");
+
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.stop();
+                    mMediaPlayer.reset();
+                }
+
+                try {
+                    mMediaPlayer.setDataSource(track.getStreamURL() + "?client_id=" + Urls.CLIENT_ID);
+                    mMediaPlayer.prepareAsync();
+                    mPlayerControl.setVisibility(View.VISIBLE);
+                    if(mMediaPlayer.isPlaying()) {
+                        togglePlayPause(1);
+                    }
+                    Log.d("Music started:","true");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -119,6 +179,11 @@ public class SearchFragment extends Fragment implements RequestResponse {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+
+                    if(!(new NetworkConnection(getContext()).isConnected())) {
+                        startActivity(new Intent(getContext(), NoInternetActivity.class));
+                    }
+
                     InputMethodManager imgr = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
                     if (imgr != null) {
                         imgr.hideSoftInputFromWindow(getView().getRootView().getWindowToken(), 0);
@@ -131,6 +196,34 @@ public class SearchFragment extends Fragment implements RequestResponse {
             }
         });
 
+        mPlayerControl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                togglePlayPause(0);
+            }
+        });
+
+        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mMediaPlayer.start();
+            }
+        });
+
+    }
+
+    private void togglePlayPause(int in) {
+        if(in==1) {
+            mPlayerControl.setImageResource(R.drawable.ic_pause);
+            return;
+        }
+        if (mMediaPlayer.isPlaying()) {
+            mMediaPlayer.pause();
+            mPlayerControl.setImageResource(R.drawable.ic_play);
+        } else {
+            mMediaPlayer.start();
+            mPlayerControl.setImageResource(R.drawable.ic_pause);
+        }
     }
 
     private void performSearch(String query) {
@@ -157,8 +250,16 @@ public class SearchFragment extends Fragment implements RequestResponse {
         rvSearchResult = view.findViewById(R.id.rvSearchResult);
         rvSearchResult.setAdapter(searchSongAdapter);
         progressBar = view.findViewById(R.id.search_progressbar);
-        rlNoSearchHistory = view.findViewById(R.id.rlNoSearchHistory);
         rvSearchResult = view.findViewById(R.id.rvSearchResult);
+        songToolbar=view.findViewById(R.id.songToolbar);
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mSelectedTrackImage=view.findViewById(R.id.selected_track_image);
+        mSelectedTrackTitle=view.findViewById(R.id.selected_track_title);
+        mPlayerControl=view.findViewById(R.id.player_control);
+        sharedPreferences = view.getContext().getSharedPreferences("com.appdev_soumitri.humbirds",Context.MODE_PRIVATE);
+        HashSet<String> set = (HashSet<String>) sharedPreferences.getStringSet("song_id",new HashSet<String>());
+        if(!set.isEmpty()) history=new ArrayList<>(set);
     }
 
     @Override
@@ -168,7 +269,9 @@ public class SearchFragment extends Fragment implements RequestResponse {
             songs.clear();
             rvSearchResult.setAdapter(searchSongAdapter);
             if(list!=null) songs.addAll(list);
-
+            else {
+                Toast.makeText(context, "No search results found :(", Toast.LENGTH_LONG).show();
+            }
             // sort songs based on assumed parameter of being more relevant a result
             Collections.sort(songs, new Comparator<SongModel>() {
                 @Override
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# DevRushers
+<<<<<<< HEAD
+=======
+
+>>>>>>> c53a840687dd1b1c30249265ec03efad31962cba
+# __HumBirds__
+
+In this world of 7.8 billion population, many people are still lonely, friendless, sad and depressed. Specially in current corona affected locked down life, people are getting more and more isolated, and having no friends to talk with leaves a large negative impact on our mental health. Though we are surrounded with thousands of unknown 'friends' in social media applications, still we don't know how many of them have similar mentality, taste and choices like us . We can get matches in dating apps, but can't convert it into long-lasting relationship due to different tastes and choices.
+
+
+So here comes our android application <b><i>HumBirds </i></b> , a musical social media platform, that enables us to connect with people of similar music taste all around the world. Connect with people, break the ice by talking about favourite artists, podcasts, or audio-books, share your thoughts, hang out at concerts, create great bondings and even try your luck at Cupid's household, all through a beautiful musical journey. HumBirds gurantees a certain level of similar taste between the connected users. So surround yourself with great alike friends from all around the world, speak your heart out, and break free from your boring lonely life.
+
+### Contributors to this project:
+1. Soumitri Chattopadhyay 
+2. Aritra Marik
+3. Sattam Bandyopadhyay
+
+
+## To debug the application in your machine: 
+
+It is preferable to have Android Studio (or IntelliJ IDE / Eclipse) installed in your device .<br>
+Clone (or download .zip and extract all) the repository.<br>
+Open <b> HumBirds - App </b> as an Android Project in your IDE. <br> 
+Run the application from your IDE either on an Emulator device or on your android smartphone by enabling Developer Options.
+  
+  <br><br>
+  
+<<<<<<< HEAD
+  #### Alternatively you can download the .apk file from the link below: <br>
+=======
+  #### Alternatively you can download the .apk file from the link below: <br>
+>>>>>>> c53a840687dd1b1c30249265ec03efad31962cba
